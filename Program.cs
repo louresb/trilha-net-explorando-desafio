4d9fcbfb46f67b842534c74afa0871ba78a159f7
louresb <bruno.loures@hotmail.com>
2022-12-16 12:08:53 -0300

    Desafio concluído!

--- Program.cs
+++ Program.cs
@@ -16,7 +16,8 @@ hospedes.Add(p2);
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+// Quantidade de diasReservados modificada para verificar desconto  
+Reserva reserva = new Reserva(diasReservados: 15);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
